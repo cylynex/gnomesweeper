5c3183d19f433564bd8b7ecd37acadcfba243f43
Santo Commarato <cylynex@gmail.com>
2018-05-08 09:50:37 -0600

    Setup minefield creation script

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    public int x;
+    public int y;
+
+    public static Tile CreateNewTile(int x, int y) {
+
+        GameObject tile = (GameObject)Instantiate(Resources.Load("Prefabs/Tile"));
+
+        GameObject tiles = GameObject.FindGameObjectWithTag("Tiles");
+
+        // Minefield obj
+        Minefield minefield = GameObject.FindGameObjectWithTag("Minefield").GetComponent<Minefield>();
+
+        tile.GetComponent<Tile>().x = x;
+        tile.GetComponent<Tile>().y = y;
+
+        // Put in tiles group
+        tile.transform.parent = tiles.transform;
+
+        // Position in the minefield
+        tile.transform.position = new Vector2((float)x - ((float)minefield.xTotal - 1f) / 2f, (float)y - ((float)minefield.yTotal - 1f) / 2f);
+        
+        return tile.GetComponent<Tile>();
+
+    }
+
+}
